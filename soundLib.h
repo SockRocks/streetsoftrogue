5538bafe92222d35b51bba86b2ba1846fdd63e51
SockRocks <67668693+SockRocks@users.noreply.github.com>
2025-02-15 19:55:49 -0500

    Documented soundLib.h

--- soundLib.h
+++ soundLib.h
@@ -2,9 +2,12 @@
 #include <mmsystem.h>
 #include <iostream>
 #include <tchar.h>
+// Links the windows multi media library as a dynamic library
 #pragma comment(lib, "winmm.lib")
 
 void laugh(){
+    // Unicode version of play sound
     PlaySoundW(L"crazy.wav", NULL, SND_FILENAME | SND_ASYNC);
+    // Allows sound to play out
     Sleep(5000);
 }
