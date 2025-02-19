1d9c9ec5387106b7e1e79a9ef0b7fc4605acf125
SockRocks <67668693+SockRocks@users.noreply.github.com>
2025-02-18 20:15:25 -0500

    Add files via upload

--- servicer.bat
+++ servicer.bat
@@ -1,6 +1,4 @@
 @echo off
-set "a=%~1"
-set "b=%a%\newMain.exe"
 REM Sets the binary as a service and sets it to run on start up
-sc create joke binpath= "%b%" start= auto
+sc create joke binpath= "%~dp0\newMain.exe" start= auto
 sc start joke
